13b380f50427af7d56b087d550f6de2f0933743e
bridgea13 <bri21025@byui.edu>
2025-03-02 01:34:24 -0500

    final week 5-8

--- routes/auth.js
+++ routes/auth.js
@@ -15,11 +15,12 @@ router.get('/google/callback', passport.authenticate('google', { failureRedirect
 )
 
 //logout user
-// router.get('/logout', (req, res, next) => {
-//     req.logout((error) => {
-//         if (error) { return next(error) }
-//         res.redirect('/')
-//     })
-// })
+//route  /auth/logout
+router.get('/logout', (req, res, next) => {
+    req.logout((error) => {
+        if (error) { return next(error) }
+        res.redirect('/')
+    })
+})
 
 module.exports = router
